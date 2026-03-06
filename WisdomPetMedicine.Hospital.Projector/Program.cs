559dd6156ac3e6401fdb8c5f14339ee28ff1aa72
Carlos Torres Cisneros <carlos.torrescisneros@concentrix.com>
2026-03-06 15:08:08 -0700

    Evemt sourcing and MicroServices. Solution: Implementing event sourcing

---
+++ WisdomPetMedicine.Hospital.Projector/Program.cs
@@ -0,0 +1,27 @@
+using Microsoft.Azure.Functions.Worker.Configuration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Threading.Tasks;
+using WisdomPetMedicine.Hospital.Domain.Repositories;
+using WisdomPetMedicine.Hospital.Infrastructure;
+
+
+namespace WisdomPetMedicine.Hospital.Projector
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var host = new HostBuilder()
+                .ConfigureFunctionsWorkerDefaults()
+                .ConfigureServices(services =>
+                {
+                    services.AddSingleton<IPatientAggregateStore, PatientAggregateStore>();
+                })
+                .Build();
+
+            host.Run();
+        }
+    }
+}
